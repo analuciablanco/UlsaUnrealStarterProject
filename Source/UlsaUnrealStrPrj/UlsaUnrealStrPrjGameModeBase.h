6b4b981d823f9b850ca5ec8a18fc47b35605695d
Ana Lucia Blanco <analuciablanco@outlook.com>
2021-02-03 13:54:05 -0700

    publish project

---
+++ Source/UlsaUnrealStrPrj/UlsaUnrealStrPrjGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UlsaUnrealStrPrjGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ULSAUNREALSTRPRJ_API AUlsaUnrealStrPrjGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
